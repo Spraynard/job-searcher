85293e7450a5b576b6fe6bcff55547d93462f67f
Spraynard <kellan.martin@gmail.com>
2017-07-21 21:04:27 -0400

    initial commit in branch

--- node_python_test.js
+++ node_python_test.js
@@ -1,7 +1,6 @@
 const exec = require('child_process').exec;
 const path = require('path');
 const USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36';
-
 let F_USER_AGENT = USER_AGENT.replace(/[();]/g, match => {
 	switch (match) {
 		case ';':
@@ -19,16 +18,17 @@ var results;
 
 function fuckItUp(IP, USER_AGENT, URL, KEYWORDS) {
 	return new Promise(( res, rej ) => {
-		exec(`python /routes/remote-api-scripts/career_jet_api_search.py ${IP} ${F_USER_AGENT} ${URL} ${KEYWORDS}`, (err, stdout, stderr) => {
+		exec(`python routes/remote-api-scripts/career_jet_api_search.py ${IP} ${F_USER_AGENT} ${URL} ${KEYWORDS}`, (err, stdout, stderr) => {
 			if (err) rej(err);
 			// console.log('stdout:', stdout, 'stderr:', stderr);
-			res(JSON.parse(stdout));
+			res(stdout);
 		})
 	})
 }
 
-Promise.resolve(fuckItUp(IP, F_USER_AGENT, URL, KEYWORDS))
+fuckItUp(IP, F_USER_AGENT, URL, KEYWORDS)
 	.then( jsonResponse => {
-		console.log(jsonResponse)
+		results = JSON.parse(jsonResponse);
+		console.log(results);
 	})
 	.then(null, console.error)
